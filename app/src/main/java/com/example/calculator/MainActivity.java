96324beb5e23f4997bef09ba57f9749c4d100b3a
Lucas <lucas.tx.reis@gmail.com>
2020-09-24 09:28:12 -0300

    Buttons/Functionalities implemented

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    // Operations implementation
+    public void add(View view){
+
+        EditText value_1 = (EditText) findViewById(R.id.input_number_1);
+        EditText value_2 = (EditText) findViewById(R.id.input_number_2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double a = Double.parseDouble(value_1.getText().toString());
+        double b = Double.parseDouble(value_2.getText().toString());
+        a += b;
+
+        String ans = (Double.toString(a));
+        result.setText(ans);
+
+
+    }
+
+    public void sub(View view){
+
+        EditText value_1 = (EditText) findViewById(R.id.input_number_1);
+        EditText value_2 = (EditText) findViewById(R.id.input_number_2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double a = Double.parseDouble(value_1.getText().toString());
+        double b = Double.parseDouble(value_2.getText().toString());
+        a -= b;
+
+        String ans = (Double.toString(a));
+        result.setText(ans);
+
+
+    }
+    public void mul(View view){
+
+        EditText value_1 = (EditText) findViewById(R.id.input_number_1);
+        EditText value_2 = (EditText) findViewById(R.id.input_number_2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double a = Double.parseDouble(value_1.getText().toString());
+        double b = Double.parseDouble(value_2.getText().toString());
+        a *= b;
+
+        String ans = (Double.toString(a));
+        result.setText(ans);
+
+
+    }
+    public void div(View view){
+
+        EditText value_1 = (EditText) findViewById(R.id.input_number_1);
+        EditText value_2 = (EditText) findViewById(R.id.input_number_2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double a = Double.parseDouble(value_1.getText().toString());
+        double b = Double.parseDouble(value_2.getText().toString());
+        a /= b;
+
+        String ans = (Double.toString(a));
+        result.setText(ans);
+
+
     }
 }
