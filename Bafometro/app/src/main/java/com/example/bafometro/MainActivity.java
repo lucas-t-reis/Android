9e88cc53cc038ec941ab51beb000dfcfcc514027
Lucas <lucas.tx.reis@gmail.com>
2020-12-01 21:26:42 -0300

    Bafômetro same app

---
+++ Bafometro/app/src/main/java/com/example/bafometro/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.bafometro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent it) {
+        super.onActivityResult(requestCode, resultCode, it);
+
+        Log.e("ACTIVITY_RESULT", "Processing request result");
+
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+            double bloodAlcohol = it.getDoubleExtra("results", -1);
+            Toast toast;
+            String classification;
+
+            Log.e("ACTIVITY_RESULT", Double.toString(bloodAlcohol) + " is the alcohol concentration");
+
+            if(bloodAlcohol > 0.2) classification = "Pessoa Alcoolizada";
+            else classification = "Pessoa NÃO Alcoolizada";
+
+            toast = Toast.makeText(this, "Taxa de Alcoolemia:" +
+                    " " + Double.toString(bloodAlcohol) +
+                    " " + classification, Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+
+
+    }
+
+
+    public void sendData(View v) {
+
+        // Getting user data from txt Input fields
+        EditText peso = (EditText) findViewById(R.id.pesoIn);
+        EditText sexo = (EditText) findViewById(R.id.sexoIn);
+        EditText copos = (EditText) findViewById(R.id.coposIn);
+        EditText jejum = (EditText) findViewById(R.id.jejumIn);
+
+        String p = peso.getText().toString();
+        String s = sexo.getText().toString();
+        String c = copos.getText().toString();
+        String j = jejum.getText().toString();
+
+
+        Bundle bundle = new Bundle();
+        bundle.putDouble("peso", Double.parseDouble(p));
+        bundle.putString("sexo", s);
+        bundle.putInt("copos", Integer.parseInt(c));
+        bundle.putString("jejum", j);
+
+
+        Intent it = new Intent(getApplicationContext(), Tela2.class);
+        it.putExtras(bundle);
+
+        startActivityForResult(it, REQUEST_CODE);
+
+    }
+
+}
---
+++ Bafometro/app/src/main/java/com/example/bafometro/Tela2.java
@@ -0,0 +1,47 @@
+package com.example.bafometro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class Tela2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+
+    }
+
+    public void processData(View v) {
+
+        Intent it = getIntent();
+        Bundle bundle = it.getExtras();
+
+        // Retrieving user inputs from MainActivity
+        double weight = bundle.getDouble("peso");
+        int glasses = bundle.getInt("copos");
+        String sex = bundle.getString("sexo");
+        String fasting = bundle.getString("jejum");
+
+        double alcohol = glasses*4.8;
+        double coef = 0;
+
+        if(fasting.equals("s")) coef = 1.1;
+        else if(sex.equals("m")) coef = 0.7;
+        else coef = 0.6;
+
+        double bloodAlcohol = alcohol / weight*coef;
+
+        it = new Intent(getApplicationContext(), MainActivity.class);
+        it.putExtra("results", bloodAlcohol);
+
+        setResult(RESULT_OK, it);
+
+        finish();
+    }
+}
